d6e6d4dbdf432a8933306aa4b6de2ddb3531767b
Mark Drehmann <markdrehmann@gmail.com>
2020-01-15 21:37:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif !phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  else
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
