43699b6e466d82fdb1696fe45656ed9cc89875f6
Mark Drehmann <markdrehmann@gmail.com>
2020-01-15 21:32:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,14 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif !phrase.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
